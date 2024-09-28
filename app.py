5a5fbadd366c44453c466d5828b0a7a0fe6dd2e0
Alex <alex@devarno.com>
2024-09-28 07:40:36 +0100

    PORT

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def home():
 
 # Default script
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000))
