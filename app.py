8cc09f703caf038f59a712fad55cd3f6d5d49265
Alex <alex@devarno.com>
2024-10-30 15:31:07 +0000

    main.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from generator import PasswordGenerator
-from starlette.requests import Request
 
 # Init
 app = FastAPI()
@@ -18,8 +16,3 @@ async def home(request: Request):
     """Generate passwords and render them in a simple HTML page."""
     passwords = generator._generate_all_passwords()
     return templates.TemplateResponse("index.html", {"request": request, "passwords": passwords})
-
-# Default script
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
