d3dc5748dae2d1eed16fb3319e389f07e89271da
Alex <alex@devarno.com>
2024-11-24 21:40:05 +0000

    uvicorn

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def http_exception_handler(request: Request, exc):
         status_code=status.HTTP_400_BAD_REQUEST,
         name="exception.html",
         context={
-            "detail": blocks[1],
+            "detail": exc,
         }
     )
 
