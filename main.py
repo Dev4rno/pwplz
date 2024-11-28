9109397426ceab8fe9069d26d2e4931216079efd
Alex <alex@devarno.com>
2024-11-28 18:58:37 +0000

    slowapi fix

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 
-
 async def rate_limit_exception_handler(request: Request, _: RateLimitExceeded):
     """Custom handler for RateLimitExceeded"""
     return templates.TemplateResponse(
@@ -55,18 +54,6 @@ async def http_exception_handler(request: Request, exc):
         context={"request": request, "detail": exc},
         status_code=status.HTTP_400_BAD_REQUEST,
     )
-
-# @app.exception_handler(RateLimitExceeded)
-# async def rate_limit_exception_handler(request: Request, _: RateLimitExceeded):
-#     """Custom handler for RateLimitExceeded"""
-#     return templates.TemplateResponse(
-#         name="exception.html",
-#         context={
-#             "request": request,
-#             "detail": get_random_rate_limit_warning(),
-#         },
-#         status_code=status.HTTP_429_TOO_MANY_REQUESTS
-#     )
 
 # Start PasswordGenerator
 generator = PasswordGenerator(
